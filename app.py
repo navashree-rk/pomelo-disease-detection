f18af281e956b471f5fb8b2c880c03ab2a76011d
Navashree <shreeaat513@gmail.com>
2026-04-07 09:48:05 +0530

    fixed model loading issue

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 import cv2
 
 app = Flask(__name__)
-model = load_model("plant_disease_model.h5")
+model = load_model("plant_disease_model.h5", compile=False)
 
 IMAGE_SIZE = (224, 224)
 
